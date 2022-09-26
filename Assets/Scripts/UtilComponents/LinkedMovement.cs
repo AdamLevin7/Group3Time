09dd8daf814e71809fb888052351f431d482ac01
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-09-25 18:20:21 -0700

    Timer shadows, etc, etc

---
+++ Assets/Scripts/UtilComponents/LinkedMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UtilComponents
+{
+    public class LinkedMovement : MonoBehaviour
+    {
+        public GameObject other;
+        public float factor;
+        private Vector3 _myStartingPos;
+        private Vector3 _otherStartingPos;
+        
+        private void Start()
+        {
+            _myStartingPos = transform.position;
+            _otherStartingPos = other.transform.position;
+        }
+        
+        private void LateUpdate()
+        {
+            transform.position = _myStartingPos + (other.transform.position - _otherStartingPos) * factor;
+        }
+    }
+}
---
+++ Assets/Scripts/UtilComponents/TextClone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UtilComponents
+{
+    public class TextClone : MonoBehaviour
+    {
+        public GameObject reference;
+        private TextMeshProUGUI _theirText;
+        private TextMeshProUGUI _myText;
+
+        private void Start()
+        {
+            _theirText = reference.GetComponent<TextMeshProUGUI>();
+            _myText = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void LateUpdate()
+        {
+            _myText.text = _theirText.text;
+        }
+    }
+}
