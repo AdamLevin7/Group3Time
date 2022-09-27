8a4cb4d6851516c5d05bf0c696b515070cd0a43c
AdamLevin7 <63608754+AdamLevin7@users.noreply.github.com>
2022-09-27 12:32:34 -0700

    pls wonk

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    public Rigidbody2D rb;
+
+    public GameObject whatIsGround;
+    private bool isOnGround;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
