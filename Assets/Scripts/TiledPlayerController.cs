973a962945e3f975c763a232dd4fb04c25d5cb2f
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-09-28 21:03:17 -0700

    start work on tile movement

---
+++ Assets/Scripts/TiledPlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class TiledPlayerController : MonoBehaviour
+{
+    public Vector2Int startTilePos;
+    public int lerpFactor;
+    
+    private void Start()
+    {
+        transform.position = new Vector3(startTilePos.x + 0.5f, startTilePos.y + 0.5f);
+        InvokeRepeating(nameof(Tenths), 0, 0.1f); // best practices: use nameof(...)
+    }
+
+    private void Tenths()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UtilComponents/SpriteMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UtilComponents
+{
+    public class SpriteMapper : MonoBehaviour
+    {
+        /**
+         * Do not add to `names` and `sprites` in your code.
+         * Instead use `additions`.
+         */
+        public string[] names;
+        public Sprite[] sprites;
+        public IDictionary<string, Sprite> additions = new Dictionary<string, Sprite>();
+        public IDictionary<string, Sprite> map = new Dictionary<string, Sprite>();
+
+        public void Rebuild()
+        {
+            if (names.Length != sprites.Length) throw new ArgumentException("Names and sprites must be same length");
+            Debug.Log("SpriteMapper Rebuild: " + additions.Count + " from additions, "
+                        + names.Length + " from names/sprites");
+            map = new Dictionary<string, Sprite>(additions);
+            for (var i = 0; i < names.Length; i++)
+            {
+                map[names[i]] = sprites[i];
+            }
+        }
+        
+        private void Start()
+        {
+            Rebuild();
+        }
+    }
+}
