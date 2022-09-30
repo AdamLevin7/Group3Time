0ef97e07f21cbb216e46df0abd14281f375bbf1a
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2022-09-29 22:48:29 -0700

    SpriteMapper library finish

--- Assets/Scripts/UtilComponents/SpriteMapper.cs
+++ Assets/Scripts/UtilComponents/SpriteMapper.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,24 +12,47 @@ namespace UtilComponents
          */
         public string[] names;
         public Sprite[] sprites;
-        public IDictionary<string, Sprite> additions = new Dictionary<string, Sprite>();
+        public IDictionary<string, Sprite> hardCodeSprites = new Dictionary<string, Sprite>();
+        public IDictionary<string, Sprite> additionalSprites = new Dictionary<string, Sprite>();
         public IDictionary<string, Sprite> map = new Dictionary<string, Sprite>();
 
-        public void Rebuild()
+        private SpriteRenderer _renderer;
+        private bool _hasStarted;
+
+        private void BuildHardCoded()
         {
             if (names.Length != sprites.Length) throw new ArgumentException("Names and sprites must be same length");
-            Debug.Log("SpriteMapper Rebuild: " + additions.Count + " from additions, "
-                        + names.Length + " from names/sprites");
-            map = new Dictionary<string, Sprite>(additions);
             for (var i = 0; i < names.Length; i++)
             {
-                map[names[i]] = sprites[i];
+                hardCodeSprites.Add(names[i], sprites[i]);
             }
+        }
+        
+        public void Rebuild()
+        {
+            Debug.Log("SpriteMapper Rebuild: " + additionalSprites.Count + " from additions, "
+                        + hardCodeSprites.Count + " from hardcoded");
+            map = new Dictionary<string, Sprite>(additionalSprites);
+            foreach (var s in hardCodeSprites)
+            {
+                map.Add(s);
+            } 
         }
         
         private void Start()
         {
+            if (_hasStarted) return;
+            _hasStarted = true;
+            _renderer = GetComponent<SpriteRenderer>();
+            BuildHardCoded();
             Rebuild();
+        }
+
+        public void Switch(string key)
+        {
+            if (!_hasStarted) Start();
+            if (!map.ContainsKey(key)) throw new ArgumentException("No sprite for key: " + key);
+            _renderer.sprite = map[key];
         }
     }
 }
