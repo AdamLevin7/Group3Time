561e98a34611733a6d4538bb48f9dfe770aaa7ee
AdamLevin7 <63608754+AdamLevin7@users.noreply.github.com>
2022-10-11 12:49:39 -0700

    bullet shoot still doesnt work

--- Assets/Scripts/BulletShoot.cs
+++ Assets/Scripts/BulletShoot.cs
@@ -9,16 +9,20 @@ public class BulletShoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = player.transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(key.SpaceBar)
-        for(int i = 0; i < 10; i++)
-        {
-            transform.Translate(new Vector2(speed, 0));
+        
+        if (Input.GetKeyDown(KeyCode.Space)){
+            for (int i = 0; i < 10; i++)
+            {
+                transform.Translate(new Vector2(speed, 0));
+            }
+            transform.position = player.transform.position;
         }
+        
     }
 }
