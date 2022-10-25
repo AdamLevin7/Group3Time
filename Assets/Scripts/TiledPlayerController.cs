0b77f247af1d190d9498e94f80c8c3da87c6578e
aric-volman <aric.volman@gmail.com>
2022-10-24 22:34:37 -0700

    Add hardcoded bounds to TiledPlayerController

--- Assets/Scripts/TiledPlayerController.cs
+++ Assets/Scripts/TiledPlayerController.cs
@@ -5,12 +5,15 @@ public class TiledPlayerController : MonoBehaviour
 {
     public Vector2Int startTilePos;
     public Vector2 actualPos;
+    
+    private float _maxX = 22.5f;
+    private float _maxY = -2.5f;
+    private float _minX = -3.5f;
+    private float _minY = 5.5f;
+
     private bool _keydown;
-    private ITimer _timer;
 
-    private float elapsed = 0.0f;
-
-    public float lerpFactor = 10.0f; // per second
+    public float lerpFactor = 0.3f; // per second
 
     public string direction = "right";
     // expects up, down, left, right
@@ -37,19 +40,24 @@ public class TiledPlayerController : MonoBehaviour
         {
 
             if (!Input.GetKey(key)) {
-                elapsed = Time.deltaTime;
                 continue;
             }
-            if (Input.GetKey(key) && elapsed <= 0.01f) {
+            if (Input.GetKey(key) && Time.deltaTime <= 0.0125f) {
                 _mapper.Switch(Data.KeyToSprite[key]);
                 // warp to prevent diagonal weirdness, TODO better solution, like input buffering?
                 direction = Data.KeyToSprite[key];
-                transform.position = new Vector3(actualPos.x, actualPos.y);
-                actualPos += Data.KeyToDirection[key];
-            }
-
-            if (elapsed >= 0.5f) {
-                elapsed = 0.0f;
+                if (transform.position.x >= _maxX && key == KeyCode.D) {
+                    transform.position = new Vector3(_maxX, actualPos.y);
+                } else if (transform.position.y <= _maxY && key == KeyCode.S) {
+                    transform.position = new Vector3(actualPos.x, _maxY);
+                } else if (transform.position.x <= _minX && key == KeyCode.A) {
+                    transform.position = new Vector3(_minX, actualPos.y);
+                } else if (transform.position.y >= _minY && key == KeyCode.W) {
+                    transform.position = new Vector3(actualPos.x, _minY);
+                } else {
+                    transform.position = new Vector3(actualPos.x, actualPos.y);
+                    actualPos += Data.KeyToDirection[key];
+                }
             }
         }
     }
